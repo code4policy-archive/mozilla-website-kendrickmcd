22887d112a4bbc8d70c665bf755c61dc4c4e953e
Kendrick McDonald <kendrickmcdonald93@gmail.com>
2022-01-07 08:59:15 -0500

    give buttons functions

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,15 @@ myHeading.textContent = 'Hello world!';
  
  // Assign the function to the onclick event on that element
  foxImage.onclick = sayOuch;
+
+ function displayfox() {
+	var sourceOfPicture = "images/firefox-icon.png";
+  	var img = document.getElementById('foximg')
+  	img.src = sourceOfPicture;
+  	img.style.display = "block";
+ }
+
+ function hidefox() {
+  	var img = document.getElementById('foximg')
+  	img.style.display = "none";
+ }
